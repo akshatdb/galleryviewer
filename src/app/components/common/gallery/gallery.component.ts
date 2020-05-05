c25138cea74003e3a9b1593babad0c5ee5e4bb4b
akshatd4 <akshatd4@gmail.com>
2020-05-05 19:40:03 +0530

    Fixed slide show

--- src/app/components/common/gallery/gallery.component.ts
+++ src/app/components/common/gallery/gallery.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit, AfterContentChecked, AfterViewChecked } from '@angular/core';
 import { trigger, transition, useAnimation, state, style } from '@angular/animations';
 import { scaleIn, scaleOut, fadeIn, fadeOut, AnimationType } from './gallery.animation';
-import { timer } from 'rxjs';
+import { timer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-gallery',
@@ -40,28 +40,60 @@ export class GalleryComponent implements OnInit {
   animationType: AnimationType;
   @Input()
   fullScreenMode: boolean = false;
+  @Input()
+  circular: boolean = true;
+  @Input()
+  slideshow: boolean = true;
+  @Input()
+  slideChangeTime: number = 5000;
   containerWidth = undefined;
   currentIndex = 0;
   totalImages = 0;
   currentImage;
   showarrow = true;
+  slideshowsub: Subscription;
   @ViewChild('galleryContainer') galleryContainer: ElementRef;
   ngOnInit(): void {
     this.animationType = AnimationType.Scale;
     timer(5000).subscribe(evt => this.showarrow = false);
+    if (this.slideshow)
+      this.startSlideShow();
   }
 
   ngOnChanges() {
     this.totalImages = this.images.length;
+    if (!this.circular)
+      this.slideshow = false;
+    this.startSlideShow();
+  }
+  startSlideShow() {
+    if (this.slideshow && this.circular && !this.slideshowsub) {
+      this.slideshowsub = timer(this.slideChangeTime, this.slideChangeTime).subscribe(res => {
+        this.nextPage();
+      });
+    }
+  }
+  stopSlideShow() {
+    if (this.slideshowsub) {
+      this.slideshowsub.unsubscribe();
+      delete this.slideshowsub;
+    }
+  }
+  ngOnDestroy() {
+    this.stopSlideShow();
   }
 
   nextPage() {
-    if (this.currentIndex < this.totalImages)
+    if (this.currentIndex < this.totalImages - 1)
       this.currentIndex++;
+    else if (this.circular)
+      this.currentIndex = 0;
   }
   prevPage() {
     if (this.currentIndex > 0)
       this.currentIndex--;
+    else if (this.circular)
+      this.currentIndex = this.totalImages - 1;
   }
 
   goFullscreen() {
